113b9ac3035416401bb1827576516144de0f3497
andychen <andy.chen@iisigroup.com>
2020-08-28 15:20:28 +0800

    init

---
+++ src/main/java/com/andy/springboot/recordmove/RecordMoveApplication.java
@@ -0,0 +1,13 @@
+package com.andy.springboot.recordmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordMoveApplication.class, args);
+	}
+
+}
